778e9eff09680a5d847ef643465a3a1ce8bf0a0f
Moreno Pinheiro <moreno.pinheiro@gmail.com>
2012-03-29 12:40:02 -0300

    too much 404 response

--- twitnerario/apps/core/tweet.py
+++ twitnerario/apps/core/tweet.py
@@ -153,14 +153,14 @@ def send_tweets(request):
     
     tweets=create_tweets(h,m)
     if not tweets:
-        raise Http404
+        return HttpResponse(strftime("%H:%M:%S")+' :/')
     
     api=twitter.Api(consumer_key='GjDAsmaMQdZdli8pDXA',consumer_secret='lONZF93DzyXPB5974GxbUmqLxyvA9ZG3bXUoliYhG8', access_token_key='397486100-T13Va0sXGROGkNpzLZBpZrZdvl2xycyJWpov4cWV',access_token_secret='5F5ExGiDQM770mQKPTai3pAlq2A9ockVsK5oqtcwM')
     #tweets=create_tweets(23,00)
     for tweet in tweets:
         api.PostUpdate(tweet)
     
-    raise Http404
+    return HttpResponse(strftime("%H:%M:%S")+' ^-^')
 
 #
 # Envia Tweets Thread

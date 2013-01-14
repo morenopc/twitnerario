b6c6968387cce0b80d5fa94af73c7b42c4c65c2d
Moreno <moreno.pinheiro@gmail.com>
2013-01-14 18:55:15 -0200

    remove database settings

--- twitnerario/settings.py
+++ twitnerario/settings.py
@@ -155,10 +155,6 @@ LOGGING = {
     }
 }
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 ###############################################################################
 # Import local settings (override)
 ###############################################################################

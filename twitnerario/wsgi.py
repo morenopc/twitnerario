a65fa83ab37dd6661d3acc34c4dbb2e9f14b930a
Moreno <moreno.pinheiro@gmail.com>
2013-01-15 22:38:13 -0200

    dotcloud uWSGI Error: Python application not found

--- twitnerario/wsgi.py
+++ twitnerario/wsgi.py
@@ -1,5 +1,4 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = '.'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
-

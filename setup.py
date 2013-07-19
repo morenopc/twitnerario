a5d9bee8a2ff62716ace13e7335abd43f6c6e430
Moreno <moreno.pinheiro@gmail.com>
2013-07-19 15:30:00 -0300

    Fix deploy script and add forgotten cron folder

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+ 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name='Twitnerario',
     version='1.0',
-    description='OpenShift App',
     author='Moreno Cunha',
     author_email='moreno.pinheiro@gmail.com',
-    url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django<=1.4'],
+    url='https://github.com/morenopc/twitnerario',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Agende e receba lembretes (tweets) com o horario que seu onibus ira passar.',
+    install_requires=open('%swsgi/openshift/requirements.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
 )

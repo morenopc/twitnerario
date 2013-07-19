7947de725cb7f1582e13b9b6a21213fc61cc4e6f
Moreno <moreno.pinheiro@gmail.com>
2013-07-19 17:49:18 -0300

    Fix some manage.py DeprecationWarning

--- .openshift/cron/minutely/send_tweet.sh
+++ .openshift/cron/minutely/send_tweet.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-LOGS_DIR=$OPENSHIFT_HOMEDIRpython-2.6/logs
-PROJECT_ROOT=$OPENSHIFT_HOMEDIRpython-2.6/repo/wsgi/openshift
+LOGS_DIR="$OPENSHIFT_HOMEDIR"python-2.6/logs
+PROJECT_ROOT="$OPENSHIFT_HOMEDIR"python-2.6/repo/wsgi/openshift
 mkdir -p $LOGS_DIR/cron-logs
 date >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
-$OPENSHIFT_HOMEDIRpython-2.6/virtenv/bin/python $PROJECT_ROOT/manage.py cron send_tweets >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
+"$OPENSHIFT_HOMEDIR"python-2.6/virtenv/bin/python $PROJECT_ROOT/manage.py cron send_tweets >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -96,7 +96,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'openshift.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_DIR, 'templates'),

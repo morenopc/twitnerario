071f4b2bd02cca0345393fe80c71f36188850f16
Moreno <moreno.pinheiro@gmail.com>
2013-07-24 11:09:14 -0300

    fix send_tweet script

--- .openshift/cron/minutely/send_tweet.sh
+++ .openshift/cron/minutely/send_tweet.sh
@@ -5,5 +5,6 @@ if [ $(($(date +%M) % 15)) -eq 0 ]; then
 	PROJECT_ROOT="$OPENSHIFT_HOMEDIR"python-2.7/repo/wsgi/openshift
 	mkdir -p $LOGS_DIR/cron-logs
 	date >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
-	"$OPENSHIFT_HOMEDIR"python-2.7/virtenv/bin/python $PROJECT_ROOT/manage.py cron send_tweets >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
+	# python $PROJECT_ROOT/manage.py cron send_tweets >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
+	echo "from core import tweet; tweet.send_tweets()" | python $PROJECT_ROOT/manage.py shell >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
 fi

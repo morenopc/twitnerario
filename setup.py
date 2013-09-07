c540a561883e507007ed2ec64c86c37360f02578
Moreno <moreno.pinheiro@gmail.com>
2013-09-06 22:45:59 -0300

    Corrige caminhos para arquivos no servidor

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 import os
 from setuptools import setup, find_packages
  
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 setup(
     name='Twitnerario',
     version='1.0',
@@ -14,5 +12,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     description='Agende e receba lembretes (tweets) com o horario que seu onibus ira passar.',
-    install_requires=open('%swsgi/openshift/requirements.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
+    install_requires=open('%swsgi/openshift/requirements.txt' % os.environ.get('OPENSHIFT_REPO_DIR')).readlines(),
 )

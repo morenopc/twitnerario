220911678e318d383b974228205281df1bf5ed9f
Moreno <moreno.pinheiro@gmail.com>
2014-03-13 20:19:20 -0300

    Fix getup-cloud (once again) virtualenv and python directories paths

--- .openshift/cron/minutely/send_mail.sh
+++ .openshift/cron/minutely/send_mail.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-source "$OPENSHIFT_HOMEDIR"python-2.7/activate_virtenv
-PROJECT_ROOT="$OPENSHIFT_HOMEDIR"python-2.7/repo/wsgi/openshift
+source "$OPENSHIFT_HOMEDIR"python/virtenv/bin/activate
+PROJECT_ROOT="$OPENSHIFT_HOMEDIR"app-root/repo/wsgi/openshift
 python $PROJECT_ROOT/manage.py send_mail
--- .openshift/cron/minutely/send_tweet.sh
+++ .openshift/cron/minutely/send_tweet.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ $(($(date +%M) % 15)) -eq 0 ]; then
-	source "$OPENSHIFT_HOMEDIR"python-2.7/activate_virtenv
-	LOGS_DIR="$OPENSHIFT_HOMEDIR"python-2.7/logs
-	PROJECT_ROOT="$OPENSHIFT_HOMEDIR"python-2.7/repo/wsgi/openshift
+	source "$OPENSHIFT_HOMEDIR"python/virtenv/bin/activate
+	LOGS_DIR="$OPENSHIFT_HOMEDIR"python/logs
+	PROJECT_ROOT="$OPENSHIFT_HOMEDIR"app-root/repo/wsgi/openshift
 	mkdir -p $LOGS_DIR/cron-logs
 	date >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
 	# python $PROJECT_ROOT/manage.py cron send_tweets >> $LOGS_DIR/cron-logs/cron_send_tweet.log 2>&1
